f2522f935b789bb320150797b8663eae2e525e18
Will Garside <worgarside@gmail.com>
2022-10-12 22:03:50 +0100

    Add exp. backoff and non-zero exit for MQTT connect errors

--- src/application/handler/mqtt.py
+++ src/application/handler/mqtt.py
@@ -5,6 +5,9 @@ from __future__ import annotations
 
 from logging import DEBUG, getLogger
 from os import getenv
+from random import uniform
+from sys import exit as sys_exit
+from time import sleep
 
 from dotenv import load_dotenv
 from paho.mqtt.client import Client
@@ -55,7 +58,18 @@ def on_disconnect(client: Client, userdata: dict[str, object], rc: int) -> None:
     _ = client, userdata, rc
 
     LOGGER.debug("MQTT Client disconnected")
-    MQTT_CLIENT.connect(MQTT_HOST)
+    try:
+        for i in range(5):
+            MQTT_CLIENT.connect(MQTT_HOST)
+            if MQTT_CLIENT.is_connected():
+                break
+            LOGGER.error("MQTT Client failed to connect, retrying...")
+            sleep(i * (1 + uniform(0, 1)))
+        else:
+            raise ConnectionError("MQTT Client failed to connect")
+    except ConnectionError:
+        # The above doesn't seem to cause a non-zero exit code, so we'll do it manually
+        sys_exit(1)
 
 
 MQTT_CLIENT.on_connect = on_connect
