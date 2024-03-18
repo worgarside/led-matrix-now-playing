e3bda4de8b01ae8fa68ef662bcadd8cfbf6bf07a
Will Garside <worgarside@gmail.com>
2024-03-18 23:13:00 +0000

    Add param IDs

--- tests/benchmark/test__ca.py
+++ tests/benchmark/test__ca.py
@@ -17,7 +17,12 @@ if TYPE_CHECKING:
         "height",
         "limit",
     ),
-    list(product([8, 16, 32, 64], [1, 10, 100, 1000, 10000, 100000, 1000000])),
+    [
+        pytest.param(height, limit, id=f"{limit} frames @ {height}x{height}")
+        for height, limit in product(
+            [8, 16, 32, 64], [1, 10, 100, 1000, 10000, 100000, 1000000]
+        )
+    ],
 )
 def test_ca(benchmark: BenchmarkFixture, height: int, limit: int) -> None:
     """Benchmark the CA."""
