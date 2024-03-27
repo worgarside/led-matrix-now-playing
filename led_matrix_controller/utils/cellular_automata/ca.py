bc76de800a34b94f5c0a55bb1d5319676fd106da
Will Botside <worgarside.dev@gmail.com>
2024-03-27 17:23:13 +0000

    Add rule-specific benchmarks (#180)
    * Add rule-specific benchmarks
    
    * Remove test values ;_;
    
    * Add frame index checks to rule benchmarks
    
    ---------
    
    Co-authored-by: Will Garside <worgarside@gmail.com>

--- led_matrix_controller/utils/cellular_automata/ca.py
+++ led_matrix_controller/utils/cellular_automata/ca.py
@@ -56,9 +56,10 @@ class Grid:
     height: int
     width: int = -1
 
-    frame_index: int = 0
+    frame_index: int = -1
 
     _RULES: ClassVar[list[tuple[TargetSliceDecVal, Callable[..., Mask], StateBase]]] = []
+    _RULE_METHODS: ClassVar[list[Callable[..., Mask]]] = []
 
     Rule: ClassVar = Callable[[Self], Mask]
 
@@ -98,6 +99,7 @@ class Grid:
                     return func(self)
 
             cls._RULES.append((target_slice, wrapper, to_state))
+            cls._RULE_METHODS.append(wrapper)
             return wrapper
 
         return decorator
