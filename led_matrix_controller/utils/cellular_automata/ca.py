34d5d58b35ef6c45d3a4676049906d4380d28efd
Will Garside <worgarside@gmail.com>
2024-03-27 11:05:16 +0000

    Move caching up a level

--- led_matrix_controller/utils/cellular_automata/ca.py
+++ led_matrix_controller/utils/cellular_automata/ca.py
@@ -166,85 +166,6 @@ class Grid:
         """Return a string representation of the grid."""
         return "\n".join(" ".join(state.char for state in row) for row in self._grid)
 
-    @lru_cache
-    @staticmethod
-    def _translate_slice_start(
-        *, current: int | None, delta: int, size: int
-    ) -> int | None:
-        """Translate the start of a slice by a given delta.
-
-        Takes into account the limit of the grid: returns None if the slice goes out of bounds in a negative
-        direction; raises an error if the slice goes out of bounds in a positive direction (because this means
-        the entire slice has gone off the grid).
-
-        Args:
-            delta (int): The translation delta.
-            current (int | None): The current start of the slice.
-            size (int): The limit of the grid (either its height or width, depending on the slice direction)
-
-        Returns:
-            int | None: The new start of the slice.
-        """
-        if delta > 0:
-            # Right/Down - can go OOB (trailing edge == off-grid in this direction)
-            new_value = (current or 0) + delta
-
-            upper_bound = (size - 1) if current is None or current >= 0 else -1
-            if new_value > upper_bound:  # Gone off grid - not good!
-                raise Grid.OutOfBoundsError(current, delta, size)
-        elif delta < 0:  # Left/Up - can't go OOB
-            if current is None:  # Immediately going off grid, but that's okay
-                new_value = None
-            else:
-                lower_bound = 0 if current >= 0 else -size
-
-                if (new_value := current + delta) < lower_bound:
-                    new_value = None
-        elif delta == 0:  # No change
-            new_value = current
-
-        return new_value
-
-    @lru_cache
-    @staticmethod
-    def _translate_slice_stop(
-        *, current: int | None, delta: int, size: int
-    ) -> int | None:
-        """Translate the stop of a slice by a given delta.
-
-        Takes into account the limit of the grid: returns None if the slice goes out of bounds in a positive
-        direction; raises an error if the slice goes out of bounds in a negative direction (because this means
-        the entire slice has gone off the grid).
-
-        Args:
-            delta (int): The translation delta.
-            current (int | None): The current stop of the slice.
-            size (int): The limit of the grid (either its height or width, depending on the slice direction)
-
-        Returns:
-            int | None: The new stop of the slice.
-        """
-        if delta > 0:
-            # Right/Down - can't go OOB (leading edge beacomes open end in this direction)
-            if current is None:
-                new_value = None
-            else:
-                upper_bound = (size - 1) if current >= 0 else -1
-
-                if (new_value := current + delta) > upper_bound:  # i.e. gone off grid
-                    new_value = None
-        elif delta < 0:
-            # Left/Up - can go OOB (trailing edge == off-grid in this direction)
-            new_value = (current or 0) + delta  # Negative number!
-
-            lower_bound = 0 if current is not None and current >= 0 else -size
-            if new_value < lower_bound:
-                raise Grid.OutOfBoundsError(current, delta, size)
-        elif delta == 0:  # No change
-            new_value = current
-
-        return new_value
-
     def translate_slice(
         self,
         slice_: TargetSlice,
@@ -261,34 +182,17 @@ class Grid:
             hrz (int, optional): The horizontal translation: positive is right, negative is left. Defaults to 0.
         """
         rows, cols = slice_
-
-        return (
-            slice(
-                Grid._translate_slice_start(
-                    current=rows.start,
-                    delta=vrt,
-                    size=self.height,
-                ),
-                Grid._translate_slice_stop(
-                    current=rows.stop,
-                    delta=vrt,
-                    size=self.height,
-                ),
-                rows.step,
-            ),
-            slice(
-                Grid._translate_slice_start(
-                    current=cols.start,
-                    delta=hrz,
-                    size=self.width,
-                ),
-                Grid._translate_slice_stop(
-                    current=cols.stop,
-                    delta=hrz,
-                    size=self.width,
-                ),
-                cols.step,
-            ),
+        return _translate_slice(
+            rows_start=rows.start,
+            rows_stop=rows.stop,
+            rows_step=rows.step,
+            cols_start=cols.start,
+            cols_stop=cols.stop,
+            cols_step=cols.step,
+            vrt=vrt,
+            hrz=hrz,
+            height=self.height,
+            width=self.width,
         )
 
     @property
@@ -299,3 +203,120 @@ class Grid:
     def __getitem__(self, key: TargetSliceDecVal) -> NDArray[np.int_]:
         """Get an item from the grid."""
         return self._grid[key]
+
+
+@lru_cache
+def _translate_slice(
+    *,
+    rows_start: int | None,
+    rows_stop: int | None,
+    rows_step: int,
+    cols_start: int | None,
+    cols_stop: int | None,
+    cols_step: int,
+    vrt: int = 0,
+    hrz: int = 0,
+    height: int,
+    width: int,
+) -> TargetSlice:
+    return (
+        slice(
+            _translate_slice_start(
+                current=rows_start,
+                delta=vrt,
+                size=height,
+            ),
+            _translate_slice_stop(
+                current=rows_stop,
+                delta=vrt,
+                size=height,
+            ),
+            rows_step,
+        ),
+        slice(
+            _translate_slice_start(
+                current=cols_start,
+                delta=hrz,
+                size=width,
+            ),
+            _translate_slice_stop(
+                current=cols_stop,
+                delta=hrz,
+                size=width,
+            ),
+            cols_step,
+        ),
+    )
+
+
+def _translate_slice_start(*, current: int | None, delta: int, size: int) -> int | None:
+    """Translate the start of a slice by a given delta.
+
+    Takes into account the limit of the grid: returns None if the slice goes out of bounds in a negative
+    direction; raises an error if the slice goes out of bounds in a positive direction (because this means
+    the entire slice has gone off the grid).
+
+    Args:
+        delta (int): The translation delta.
+        current (int | None): The current start of the slice.
+        size (int): The limit of the grid (either its height or width, depending on the slice direction)
+
+    Returns:
+        int | None: The new start of the slice.
+    """
+    if delta > 0:
+        # Right/Down - can go OOB (trailing edge == off-grid in this direction)
+        new_value = (current or 0) + delta
+
+        upper_bound = (size - 1) if current is None or current >= 0 else -1
+        if new_value > upper_bound:  # Gone off grid - not good!
+            raise Grid.OutOfBoundsError(current, delta, size)
+    elif delta < 0:  # Left/Up - can't go OOB
+        if current is None:  # Immediately going off grid, but that's okay
+            new_value = None
+        else:
+            lower_bound = 0 if current >= 0 else -size
+
+            if (new_value := current + delta) < lower_bound:
+                new_value = None
+    elif delta == 0:  # No change
+        new_value = current
+
+    return new_value
+
+
+def _translate_slice_stop(*, current: int | None, delta: int, size: int) -> int | None:
+    """Translate the stop of a slice by a given delta.
+
+    Takes into account the limit of the grid: returns None if the slice goes out of bounds in a positive
+    direction; raises an error if the slice goes out of bounds in a negative direction (because this means
+    the entire slice has gone off the grid).
+
+    Args:
+        delta (int): The translation delta.
+        current (int | None): The current stop of the slice.
+        size (int): The limit of the grid (either its height or width, depending on the slice direction)
+
+    Returns:
+        int | None: The new stop of the slice.
+    """
+    if delta > 0:
+        # Right/Down - can't go OOB (leading edge beacomes open end in this direction)
+        if current is None:
+            new_value = None
+        else:
+            upper_bound = (size - 1) if current >= 0 else -1
+
+            if (new_value := current + delta) > upper_bound:  # i.e. gone off grid
+                new_value = None
+    elif delta < 0:
+        # Left/Up - can go OOB (trailing edge == off-grid in this direction)
+        new_value = (current or 0) + delta  # Negative number!
+
+        lower_bound = 0 if current is not None and current >= 0 else -size
+        if new_value < lower_bound:
+            raise Grid.OutOfBoundsError(current, delta, size)
+    elif delta == 0:  # No change
+        new_value = current
+
+    return new_value
